53916ed2cb1295831ec1aa036b5f7b32a66380a7
croeck <git@roecky.net>
2013-04-02 13:42:48 +0200

    tomcat55 now has a home dir for active bpel logging

--- packages/tomcat5.5.rb
+++ packages/tomcat5.5.rb
@@ -69,7 +69,7 @@ end
 
 package :tomcat5_user do
   noop do
-    pre :install, "useradd tomcat55"
+    pre :install, "useradd -d /home/tomcat55 tomcat55"
   end
   verify do
     has_user "tomcat55"
