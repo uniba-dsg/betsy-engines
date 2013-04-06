bf1d39c39455fab6a46373058e273298793a19c8
Cedric Röck <git@roecky.net>
2013-04-06 18:04:25 +0200

    default deployment to localhost, simple configuration would be to set the ssh port (commented out) and redirect in virtualbox from this port to port 22

--- deploy.rb
+++ deploy.rb
@@ -1,4 +1,5 @@
 default_run_options[:pty] = true
+# ssh_options[:port] = 2222
 
 # where our ssh_key is kept, update if moved/names in a nonstandard place
 # ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "id_rsa")]
@@ -9,7 +10,7 @@ set :user, 'betsy'
 set :password, 'Betsy+VB13'
 
 # machine we want to install the reef node software stack on
-role :target_box, "192.168.2.120"
+role :target_box, "localhost"
 
 # we can also have a list of machines to install them all in parrell
 #role :target_box, "192.168.100.80", "192.168.100.70", "192.168.100.14"
